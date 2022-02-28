3aa3031bd6a48a30c214b6a8904642c415831d1c
Eri Schon <erischon@gmail.com>
2022-02-28 10:07:08 +0100

    Update Work.js

--- src/components/Work.js
+++ src/components/Work.js
@@ -11,61 +11,37 @@ class Work extends Component {
 
             <div className="work__filters">
                 <span className="work__item active-work" data-filter="all">All</span>
-                <span className="work__item" data-filter=".web">Web</span>
-                <span className="work__item" data-filter=".movil">Movil</span>
-                <span className="work__item" data-filter=".design">Design</span>
+                <span className="work__item" data-filter=".web">Appli Web</span>
+                <span className="work__item" data-filter=".refonte">Refonte</span>
+                <span className="work__item" data-filter=".entretien">Entretien</span>
             </div>
 
             <div className="work__container container grid">
                 <div className="work__card mix web">
                     <img src="assets/img/work1.jpg" alt="" className="work__img" />
 
-                    <h3 className="work__title">Web Design</h3>
+                    <h3 className="work__title">Appli Web</h3>
 
                     <a href="#" className="work__button">
                         Demo <i className='bx bx-right-arrow-alt work__icon' ></i>
                     </a>
                 </div>
 
-                <div className="work__card mix movil">
+                <div className="work__card mix refonte">
                     <img src="assets/img/work2.jpg" alt="" className="work__img" />
 
-                    <h3 className="work__title">App Movil</h3>
+                    <h3 className="work__title">Refonte</h3>
 
                     <a href="#" className="work__button">
                         Demo <i className='bx bx-right-arrow-alt work__icon' ></i>
                     </a>
                 </div>
 
-                <div className="work__card mix design">
-                    <img src="assets/img/work3.jpg" alt="" className="work__img" />
 
-                    <h3 className="work__title">Brand Design</h3>
 
-                    <a href="#" className="work__button">
-                        Demo <i className='bx bx-right-arrow-alt work__icon' ></i>
-                    </a>
-                </div>
 
-                <div className="work__card mix web">
-                    <img src="assets/img/work4.jpg" alt="" className="work__img" />
 
-                    <h3 className="work__title">Web Design</h3>
 
-                    <a href="#" className="work__button">
-                        Demo <i className='bx bx-right-arrow-alt work__icon' ></i>
-                    </a>
-                </div>
-
-                <div className="work__card mix movil">
-                    <img src="assets/img/work5.jpg" alt="" className="work__img" />
-
-                    <h3 className="work__title">App Movil</h3>
-
-                    <a href="#" className="work__button">
-                        Demo <i className='bx bx-right-arrow-alt work__icon' ></i>
-                    </a>
-                </div>
             </div>
             </section>
         );
