31200d30bfde464120750a76a932ae302f7328c4
Eri Schon <erischon@gmail.com>
2022-02-28 09:50:49 +0100

    Create Services.js

---
+++ src/components/Services.js
@@ -0,0 +1,185 @@
+import React from 'react'
+
+function Services() {
+  return (
+    <section className="services section">
+        <span className="section__subtitle">Mes Services</span>
+        <h2 className="section__title">What I offer</h2>
+
+        <div className="services__container container grid">
+            <div className="services__card">
+                <h3 className="services__title">Refonte de <br /> Site Web</h3>
+
+                <span className="services__button">
+                    See more <i className='bx bx-right-arrow-alt services__icon' ></i>
+                </span>
+
+                <div className="services__modal">
+                    <div className="services__modal--content">
+                        <i className='bx bx-x services__modal--close'></i>
+
+                        <h3 className="services__modal--title">Product Designer</h3>
+                        <p className="services__modal--description">
+                            Service with more than 3 years of experience. 
+                            Providing quality work to clients and companies.
+                        </p>
+                        
+                        <ul className="services__modal--list">
+                            <li className="services__modal--item">
+                                <i className='bx bx-check services__modal--icon' ></i>
+                                <p className="services__modal--info">
+                                    I develop the user interface.
+                                </p>
+                            </li>
+
+                            <li className="services__modal--item">
+                                <i className='bx bx-check services__modal--icon' ></i>
+                                <p className="services__modal--info">
+                                    I develop the user interface.
+                                </p>
+                            </li>
+
+                            <li className="services__modal--item">
+                                <i className='bx bx-check services__modal--icon' ></i>
+                                <p className="services__modal--info">
+                                    I develop the user interface.
+                                </p>
+                            </li>
+
+                            <li className="services__modal--item">
+                                <i className='bx bx-check services__modal--icon' ></i>
+                                <p className="services__modal--info">
+                                    I develop the user interface.
+                                </p>
+                            </li>
+
+                            <li className="services__modal--item">
+                                <i className='bx bx-check services__modal--icon' ></i>
+                                <p className="services__modal--info">
+                                    I develop the user interface.
+                                </p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div className="services__card">
+                <h3 className="services__title">Ui/Ux <br /> Designer</h3>
+
+                <span className="services__button">
+                    See more <i className='bx bx-right-arrow-alt services__icon' ></i>
+                </span>
+
+                <div className="services__modal">
+                    <div className="services__modal--content">
+                        <i className='bx bx-x services__modal--close'></i>
+
+                        <h3 className="services__modal--title">Ui/UX Designer</h3>
+                        <p className="services__modal--description">
+                            Service with more than 3 years of experience. 
+                            Providing quality work to clients and companies.
+                        </p>
+                        
+                        <ul className="services__modal--list">
+                            <li className="services__modal--item">
+                                <i className='bx bx-check services__modal--icon' ></i>
+                                <p className="services__modal--info">
+                                    I develop the user interface.
+                                </p>
+                            </li>
+
+                            <li className="services__modal--item">
+                                <i className='bx bx-check services__modal--icon' ></i>
+                                <p className="services__modal--info">
+                                    I develop the user interface.
+                                </p>
+                            </li>
+
+                            <li className="services__modal--item">
+                                <i className='bx bx-check services__modal--icon' ></i>
+                                <p className="services__modal--info">
+                                    I develop the user interface.
+                                </p>
+                            </li>
+
+                            <li className="services__modal--item">
+                                <i className='bx bx-check services__modal--icon' ></i>
+                                <p className="services__modal--info">
+                                    I develop the user interface.
+                                </p>
+                            </li>
+
+                            <li className="services__modal--item">
+                                <i className='bx bx-check services__modal--icon' ></i>
+                                <p className="services__modal--info">
+                                    I develop the user interface.
+                                </p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div className="services__card">
+                <h3 className="services__title">Visual <br /> Designer</h3>
+
+                <span className="services__button">
+                    See more <i className='bx bx-right-arrow-alt services__icon' ></i>
+                </span>
+
+                <div className="services__modal">
+                    <div className="services__modal--content">
+                        <i className='bx bx-x services__modal--close'></i>
+
+                        <h3 className="services__modal--title">Visual designer</h3>
+                        <p className="services__modal--description">
+                            Service with more than 3 years of experience. 
+                            Providing quality work to clients and companies.
+                        </p>
+                        
+                        <ul className="services__modal--list">
+                            <li className="services__modal--item">
+                                <i className='bx bx-check services__modal--icon' ></i>
+                                <p className="services__modal--info">
+                                    I develop the user interface.
+                                </p>
+                            </li>
+
+                            <li className="services__modal--item">
+                                <i className='bx bx-check services__modal--icon' ></i>
+                                <p className="services__modal--info">
+                                    I develop the user interface.
+                                </p>
+                            </li>
+
+                            <li className="services__modal--item">
+                                <i className='bx bx-check services__modal--icon' ></i>
+                                <p className="services__modal--info">
+                                    I develop the user interface.
+                                </p>
+                            </li>
+
+                            <li className="services__modal--item">
+                                <i className='bx bx-check services__modal--icon' ></i>
+                                <p className="services__modal--info">
+                                    I develop the user interface.
+                                </p>
+                            </li>
+
+                            <li className="services__modal--item">
+                                <i className='bx bx-check services__modal--icon' ></i>
+                                <p className="services__modal--info">
+                                    I develop the user interface.
+                                </p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Services
