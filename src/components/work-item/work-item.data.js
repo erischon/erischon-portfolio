deea45143bceb7da069c78e68c4f26b286d9872f
Eri Schon <erischon@gmail.com>
2022-02-28 21:29:57 +0100

    work

--- src/components/work-item/work-item.data.js
+++ src/components/work-item/work-item.data.js
@@ -3,7 +3,7 @@ export const workData = [
         id: "1",
         name: "What Colors",
         type: "Appli",
-        image: "work1.jpg",
+        image: "whatcolor-640x420-1.jpg",
         liveUrl: "https://whatcolors.netlify.app/",
         backUrl: "https://github.com/erischon/whatcolors-back",
         frontUrl: "https://github.com/erischon/whatcolors-front",
@@ -11,11 +11,12 @@ export const workData = [
     },
     {
         id: "2",
-        name: "What Colors",
+        name: "Portfolio",
         type: "Appli",
-        image: "work1.jpg",
-        demoUrl: "https://whatcolors.netlify.app/",
-        githubUrl: "",
-        tags: ["React", "Node.js", "Express", "PostgreSQL", "API REST"]
+        image: "portfolio-640x420-1.jpg",
+        demoUrl: "https://erischon-portfolio.netlify.app/",
+        backUrl: "",
+        frontUrl: "https://github.com/erischon/erischon-portfolio",
+        tags: ["HTML", "CSS", "Gatsby"]
     }
 ]
