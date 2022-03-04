49973d0dc77724e6c4bacac56a13d8dfbaff8cbb
Eri Schon <erischon@gmail.com>
2022-03-04 16:08:41 +0100

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,17 +1,14 @@
 import React from 'react'
 
 import { Helmet } from 'react-helmet'
-import { withPrefix } from 'gatsby'
 
 import "../assets/css/main.css"
-
 
 const Layout = ({ children }) => {
   return (
     <React.Fragment>
       <Helmet>
         <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet' />
-        <script src={withPrefix("/js/main.js")} />    
       </Helmet>
       
         { children }
