af42920f3d656be931249f40cac9bf8e4c0ff77d
Eri Schon <erischon@gmail.com>
2022-03-06 10:43:01 +0100

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ function Home() {
         <div className="home__data">
             <span className="home__greeting">Bonjour, je suis</span>
             <h1 className="home__name">Eri Schön</h1>
-            <h3 className="home__education">Développeur d'Application Web Freelance</h3>
+            <h3 className="home__education">Développeur d'Applications Web</h3>
 
             <div className="home__buttons">
                 {/* 
